7fd1a82bbfa0fb0d7081f44c225af772648e9c5e
piyushingale23 <piyush.ingale@sarvatra.in>
2026-06-29 15:08:49 +0530

    jenkins v3

--- src/com/core/java/jenkins/Jenkins.java
+++ src/com/core/java/jenkins/Jenkins.java
@@ -4,6 +4,6 @@ public class Jenkins {
 
     public static void main(String[] args) {
 
-        System.out.println("Integrated with jenkins v2");
+        System.out.println("Integrated with jenkins v3");
     }
 }
